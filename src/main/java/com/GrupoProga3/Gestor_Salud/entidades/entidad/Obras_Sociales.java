ebef6de8c783ccfce60e4feb096432d3f4c4ccec
mike19661978 <mleccese1966@gmail.com>
2026-04-30 11:39:26 -0300

    Agregada entidad Obras Sociales.

--- src/main/java/com/GrupoProga3/Gestor_Salud/entidades/entidad/Obras_Sociales.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/entidades/entidad/Obras_Sociales.java
@@ -1,9 +1,17 @@
 package com.GrupoProga3.Gestor_Salud.entidades.entidad;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "Obras_Sociales")
 
 public class Obras_Sociales {
     @Id
@@ -13,6 +21,7 @@ public class Obras_Sociales {
 
     @Column(name= "nombre", nullable = false, length =100)
     private String nombre;
+
     @Column (name= "cobertura", nullable = false, length=100)
     private String cobertura;
 
