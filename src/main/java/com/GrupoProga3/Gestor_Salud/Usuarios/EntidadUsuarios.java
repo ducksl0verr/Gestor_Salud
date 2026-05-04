be98e399742a6b98501e28dc32cd3d7c8096eba2
maxif <maxiferrarello@gmail.com>
2026-05-04 10:47:43 -0300

    commit para que ande

--- src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/EntidadUsuarios.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/EntidadUsuarios.java
@@ -1,7 +1,6 @@
 package com.GrupoProga3.Gestor_Salud.Usuarios;
 
 
-import com.GrupoProga3.Gestor_Salud.Domicilio.EntidadDomicilio;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -53,9 +52,6 @@ public class EntidadUsuarios {
 
 
     /// Aca segun por lo que entiendo, un domicilio pueden tener muchos usuarios pero un usuario puede tener un solo domicilio
-    @ManyToOne
-    @JoinColumn(name = "id_domicilio")
-    private EntidadDomicilio domicilio;
 
 }
 
