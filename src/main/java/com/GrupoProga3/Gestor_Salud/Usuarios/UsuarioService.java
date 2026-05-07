c6756f91050b65c1ff6aed9c8bf8170a3a5dc0d6
maxif <maxiferrarello@gmail.com>
2026-05-07 09:18:42 -0300

    hola

---
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.GrupoProga3.Gestor_Salud.Usuarios;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private ModelMapper modelMapper;
+
+}
