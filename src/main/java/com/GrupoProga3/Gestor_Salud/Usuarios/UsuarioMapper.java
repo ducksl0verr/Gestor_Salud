325f6d163363e268b435935ab986fb83de374e04
maxif <maxiferrarello@gmail.com>
2026-05-12 11:55:05 -0300

    union

--- src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/UsuarioMapper.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/UsuarioMapper.java
@@ -1,9 +1,13 @@
 package com.GrupoProga3.Gestor_Salud.Usuarios;
 
 
+import com.GrupoProga3.Gestor_Salud.Domicilio.EntidadDomicilio;
+
 @org.mapstruct.Mapper(componentModel = "Spring")
 public interface UsuarioMapper {
 
     UsuarioDTO ToDto(EntidadUsuarios usuario);
     EntidadUsuarios ToEntity(UsuarioDTO dto);
+
+    EntidadDomicilio lk();
 }
