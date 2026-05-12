a5f939a23c76cd17965a4b2b7b3648f2ea6b788a
Dan't <dan.sanchezz666@gmail.com>
2026-05-12 19:03:14 -0300

    Creación de servicio, controlador, dto y mapper para entidad domicilio.

---
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Domicilio/IServicioDomicilio.java
@@ -0,0 +1,13 @@
+package com.GrupoProga3.Gestor_Salud.Domicilio;
+
+import com.GrupoProga3.Gestor_Salud.Domicilio.Dominio.DTO.DomicilioDTO;
+
+import java.util.List;
+
+public interface IServicioDomicilio {
+    DomicilioDTO guardar (DomicilioDTO domicilioDTO);
+    void borrar (Long id);
+    DomicilioDTO buscarPorId(Long id);
+    DomicilioDTO actualizar (Long id, DomicilioDTO domicilioDTO);
+    List<DomicilioDTO> buscarTodos();
+}
