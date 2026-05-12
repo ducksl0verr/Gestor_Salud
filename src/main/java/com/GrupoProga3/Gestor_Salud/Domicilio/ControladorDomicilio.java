066a962ff47330d12321f3ae89d5bd74cf7d52f4
Dan't <dan.sanchezz666@gmail.com>
2026-05-12 19:03:14 -0300

    Creación de servicio, controlador, dto y mapper para entidad domicilio.

---
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Domicilio/ControladorDomicilio.java
@@ -0,0 +1,35 @@
+package com.GrupoProga3.Gestor_Salud.Domicilio;
+
+import com.GrupoProga3.Gestor_Salud.Domicilio.Dominio.DTO.DomicilioDTO;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/domicilio")
+public class ControladorDomicilio {
+    private final IServicioDomicilio servicioDomicilio;
+    @GetMapping
+    public ResponseEntity<List<DomicilioDTO>> buscarTodos (){
+        return ResponseEntity.ok(servicioDomicilio.buscarTodos());
+    }
+    @GetMapping("/{idDomicilio}")
+    public ResponseEntity<DomicilioDTO> buscarPorId (@PathVariable Long idDomicilio) {
+        return  ResponseEntity.ok(servicioDomicilio.buscarPorId(idDomicilio));
+    }
+    /*@PostMapping          Pensadolo bien, realmente no es tan necesario un método http para CREAR un domicilio,
+                            ni tampoco uno para borrar (por eso no lo agregue). Aunque simpre se puede chequear
+    public ResponseEntity<DomicilioDTO> crearDomicilio (@RequestBody @Valid DomicilioDTO domicilioDTO) {
+        return new ResponseEntity<>(servicioDomicilio.guardar(domicilioDTO), HttpStatus.CREATED);
+    }
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<DomicilioDTO> actualizarDomicilio (@PathVariable Long id, @RequestBody @Valid DomicilioDTO domicilioDTO) {
+        return ResponseEntity.ok(servicioDomicilio.actualizar(id,domicilioDTO));
+    }
+}
---
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Domicilio/ServicioDomicilio.java
@@ -0,0 +1,53 @@
+package com.GrupoProga3.Gestor_Salud.Domicilio;
+
+import com.GrupoProga3.Gestor_Salud.Domicilio.Dominio.DTO.DomicilioDTO;
+import com.GrupoProga3.Gestor_Salud.Domicilio.Dominio.Mappers.DomicilioMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class ServicioDomicilio implements IServicioDomicilio{
+
+    private final RepositorioDomicilio repositorioDomicilio;
+    private final DomicilioMapper domicilioMapper;
+
+    @Override
+    public DomicilioDTO guardar(DomicilioDTO domicilioDTO) {
+        EntidadDomicilio guardado =repositorioDomicilio.save(domicilioMapper.toEntity(domicilioDTO));
+        System.out.println(guardado);
+        return domicilioMapper.toDto(guardado);
+    }
+
+    @Override
+    public void borrar(Long id) {
+        this.repositorioDomicilio.findById(id).ifPresent(repositorioDomicilio::delete);
+    }
+
+    @Override
+    public DomicilioDTO buscarPorId(Long id) {
+        return repositorioDomicilio.findById(id)
+                .map(domicilioMapper::toDto)
+                .orElseThrow(); ///acá deberíamos agregar una excepción personalizada, recordar hacerlas
+    }
+
+    @Override
+    public DomicilioDTO actualizar(Long id, DomicilioDTO domicilioDTO) {
+        EntidadDomicilio dom = repositorioDomicilio.findById(id)
+                .orElseThrow();
+        dom.setCalle(domicilioDTO.calle());
+        dom.setDepto(domicilioDTO.depto());
+        dom.setPiso(domicilioDTO.piso());
+        dom.setLocalidad(domicilioDTO.localidad());
+        dom.setCodigo_postal(domicilioDTO.codigo_postal());
+        dom.setProvincia(domicilioDTO.provincia());
+        EntidadDomicilio actualizado = repositorioDomicilio.save(dom);
+        return domicilioMapper.toDto(actualizado);
+    }
+
+    @Override
+    public List<DomicilioDTO> buscarTodos() {
+        return repositorioDomicilio.findAll().stream().map(domicilioMapper::toDto).toList();
+    }
+}
