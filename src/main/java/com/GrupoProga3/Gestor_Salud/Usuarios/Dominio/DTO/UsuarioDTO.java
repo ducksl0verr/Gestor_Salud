e1432b278cfdeaf7c3173a0a8198baaad5af111a
maxif <maxiferrarello@gmail.com>
2026-05-22 10:51:33 -0300

    correccion

--- src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/Dominio/DTO/UsuarioDTO.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Usuarios/Dominio/DTO/UsuarioDTO.java
@@ -1,11 +1,18 @@
 package com.GrupoProga3.Gestor_Salud.Usuarios.Dominio.DTO;
 
-public record UsuarioDTO( String nombre,
-         String apellido,
-         String dni,
-         String telefono,
-         String matricula, // posible cambio debido a que puede ser null si no es profesional. VER
-         String email){ // elimine el atributo de id_domicilioo FK, ver en usuarioMapperImpl.java si hay que cambiar, se encuentra en la carpeta target.
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record UsuarioDTO(@NotBlank String nombre,
+                         @NotBlank String apellido,
+                         @NotBlank @Pattern(regexp = "\\d{7,8}", message = "DNI inválido") String dni,
+                         @NotBlank @Pattern(
+                                 regexp = "^[0-9+\\-\\s]{6,20}$",
+                                 message = "El teléfono tiene un formato inválido"
+                         )String telefono,
+                         @NotBlank String matricula, // posible cambio debido a que puede ser null si no es profesional. VER
+                         @NotBlank @Email String email){ // elimine el atributo de id_domicilioo FK, ver en usuarioMapperImpl.java si hay que cambiar, se encuentra en la carpeta target.
 }
 
 
