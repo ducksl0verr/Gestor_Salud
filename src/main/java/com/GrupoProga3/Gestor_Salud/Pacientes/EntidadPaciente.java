207ae545169999e85f8e6d62d12f390a573882f0
maxi <maxiferrarello@gmail.com>
2026-05-24 01:18:32 -0300

    correcciones

--- src/main/java/com/GrupoProga3/Gestor_Salud/Pacientes/EntidadPaciente.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Pacientes/EntidadPaciente.java
@@ -2,7 +2,6 @@ package com.GrupoProga3.Gestor_Salud.Pacientes;
 
 import com.GrupoProga3.Gestor_Salud.Domicilio.Dominio.EntidadDomicilio;
 import com.GrupoProga3.Gestor_Salud.ObraSocial.EntidadObraSocial;
-import com.GrupoProga3.Gestor_Salud.Pago.EntidadPago;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,7 +10,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Getter
@@ -20,38 +18,21 @@ import java.util.List;
 @NoArgsConstructor
 @Table(name = "pacientes")
 public class EntidadPaciente {
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
     @Column(name = "nombre_paciente", nullable = false, length = 50)
     private String nombre;
-
-
-    @NotBlank
     @Column(name = "apellido_paciente", nullable = false, length = 50)
     private String apellido;
-
-
-    @NotBlank
     @Column(name = "fecha_nacimiento", nullable = false)
     @Temporal(TemporalType.DATE)
     private Date fecha_nacimiento;
 
-
     //RELACIONES
-
     @ManyToOne
     @JoinColumn(name = "id_domicilio")
     private EntidadDomicilio domicilio;
-
-    @OneToMany
-    @JoinColumn(name="id_pago")
-    private List<EntidadPago> pagos;
-
 
     @ManyToOne
     @JoinColumn(name = "id_obra_social")
