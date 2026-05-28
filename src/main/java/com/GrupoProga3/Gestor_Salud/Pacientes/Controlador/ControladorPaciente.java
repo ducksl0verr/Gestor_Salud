183b58c7cde659a91094964a9d2f3ffd959768fe
maxif <maxiferrarello@gmail.com>
2026-05-28 09:07:28 -0300

    Cambios en proceso

--- src/main/java/com/GrupoProga3/Gestor_Salud/Pacientes/Controlador/ControladorPaciente.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Pacientes/Controlador/ControladorPaciente.java
@@ -2,13 +2,13 @@ package com.GrupoProga3.Gestor_Salud.Pacientes.Controlador;
 
 import com.GrupoProga3.Gestor_Salud.Pacientes.Dominio.DTO.PacienteDTO;
 import com.GrupoProga3.Gestor_Salud.Pacientes.Servicio.IServicioPaciente;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -22,6 +22,37 @@ public class ControladorPaciente {
     {
         return ResponseEntity.ok(servicioPaciente.buscarPorid(id));
     }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteDTO>> buscarTodos()
+    {
+        return ResponseEntity.ok(servicioPaciente.buscarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> guardar (@RequestBody @Valid PacienteDTO pacienteDTO)
+    {
+        return new ResponseEntity<>(servicioPaciente.guardar(pacienteDTO), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrar(@PathVariable Long id)
+    {
+        servicioPaciente.borrar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteDTO> actualizar (@PathVariable Long id, PacienteDTO pacienteDTO)
+    {
+        return ResponseEntity.ok(servicioPaciente.actualizar(id,pacienteDTO));
+    }
+
+
+
+
+
+
 
 
 }
