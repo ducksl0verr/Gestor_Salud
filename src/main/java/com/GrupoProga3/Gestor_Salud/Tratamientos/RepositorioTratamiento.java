952ea1fa48c304f52150afcf32e2158f593d473a
Dan't <dan.sanchezz666@gmail.com>
2026-05-31 17:24:50 -0300

    Creación de: el servicio, controlador, repositorio, dtos y mapper de la entidad turno; excepiones personalizadas para la entidad sala, tratamiento y tuno. Actualización del controlador de historial clinico, de la entidad sala y tratamiento

--- src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/RepositorioTratamiento.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/RepositorioTratamiento.java
@@ -1,6 +1,5 @@
 package com.GrupoProga3.Gestor_Salud.Tratamientos;
 
-import com.GrupoProga3.Gestor_Salud.Tratamientos.Doiminio.EntidadTratamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
