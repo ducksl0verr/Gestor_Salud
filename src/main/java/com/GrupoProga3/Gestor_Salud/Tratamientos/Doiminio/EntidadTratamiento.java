708ec16cf3f1fc46f5c66f1fb9f8a2294b359908
Dan't <dan.sanchezz666@gmail.com>
2026-06-02 12:58:08 -0300

    Creación de dtos, mapper y servicio de tratamiento

--- src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/Doiminio/EntidadTratamiento.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/Doiminio/EntidadTratamiento.java
@@ -1,12 +1,10 @@
-package com.GrupoProga3.Gestor_Salud.Tratamientos;
+package com.GrupoProga3.Gestor_Salud.Tratamientos.Doiminio;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
-@Table(name = "Tratamientos")
+@Table(name = "tratamientos")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,12 +16,10 @@ public class EntidadTratamiento {
     @Column(name = "id_tratamiento")
     private Long id;
 
-    @NotBlank(message = "El nombre del tratamiento no puede estar vacío")
-    @Size(min = 2, max = 150, message = "El nombre debe tener entre 2 y 150 caracteres")
     @Column(name = "nombre", nullable = false, length = 150)
     private String nombre;
 
-    @Size(max = 500, message = "La descripción no puede superar los 500 caracteres")
     @Column(name = "descripcion", length = 500)
     private String descripcion;
+
 }
