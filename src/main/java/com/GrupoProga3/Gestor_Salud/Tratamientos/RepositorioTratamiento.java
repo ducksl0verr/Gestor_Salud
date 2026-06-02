5e17b43670bbf8aee61f26bb1177e37bcb452da0
Dan't <dan.sanchezz666@gmail.com>
2026-06-02 12:58:08 -0300

    Creación de dtos, mapper y servicio de tratamiento

--- src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/RepositorioTratamiento.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/Tratamientos/RepositorioTratamiento.java
@@ -1,5 +1,6 @@
 package com.GrupoProga3.Gestor_Salud.Tratamientos;
 
+import com.GrupoProga3.Gestor_Salud.Tratamientos.Doiminio.EntidadTratamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
