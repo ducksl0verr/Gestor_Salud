4c2c27bc851ded715429781cc2a2096d43772b87
mike19661978 <mleccese1966@gmail.com>
2026-06-10 22:05:33 -0300

    se cambio controlador

--- src/main/java/com/GrupoProga3/Gestor_Salud/ObraSocial/ControladorObraSocial.java
+++ src/main/java/com/GrupoProga3/Gestor_Salud/ObraSocial/ControladorObraSocial.java
@@ -1,15 +1,46 @@
 package com.GrupoProga3.Gestor_Salud.ObraSocial;
 
-import com.GrupoProga3.Gestor_Salud.ObraSocial.Dominio.DTO.ObraSocialDTO;
-import com.GrupoProga3.Gestor_Salud.ObraSocial.Dominio.DTO.ObraSocialRespuesta;
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 
-@AllArgsConstructor
+import com.GrupoProga3.Gestor_Salud.ObraSocial.Dominio.DTO.ObraSocialNueva;
+import com.GrupoProga3.Gestor_Salud.ObraSocial.Dominio.DTO.ObraSocialRespuesta;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/obras-sociales")
+@RequiredArgsConstructor
 public class ControladorObraSocial {
+
     private final ServicioObraSocial servicioObraSocial;
 
-    public ResponseEntity<ObraSocialRespuesta> buscarPorId (Long id){
-        return ResponseEntity.ok(servicioObraSocial.buscarPorId(id));
+    @PostMapping
+    public ResponseEntity<ObraSocialRespuesta> guardar(
+            @Valid @RequestBody ObraSocialNueva nueva) {
+
+        ObraSocialRespuesta respuesta = servicioObraSocial.guardar(nueva);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(respuesta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ObraSocialRespuesta>> buscarObraSocial(
+            @RequestParam(required = false) String nombre,
+            @RequestParam(required = false) String codigo) {
+
+        return ResponseEntity.ok(
+                servicioObraSocial.buscarObraSocial(nombre, codigo)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrar(@PathVariable Long id) {
+        servicioObraSocial.borrar(id);
+        return ResponseEntity.noContent().build();
     }
 }
