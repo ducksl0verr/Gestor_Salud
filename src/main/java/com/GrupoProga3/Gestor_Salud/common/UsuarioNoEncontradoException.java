7733b0be93546a0381fd871cadb21790a06d383a
Dan't <dan.sanchezz666@gmail.com>
2026-06-13 12:35:48 -0300

    Eliminación de excepción "usuarioNoEncontrado"

--- src/main/java/com/GrupoProga3/Gestor_Salud/common/UsuarioNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.GrupoProga3.Gestor_Salud.common;
-
-public class UsuarioNoEncontradoException extends RuntimeException {
-    public UsuarioNoEncontradoException(String message) {
-        super(message);
-    }
-}
